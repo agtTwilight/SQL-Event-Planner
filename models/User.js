4531e54707c3e0e747cc6715c3f753a2916a1f8c
agtTwilight <lukasmacmillen@gmail.com>
2023-04-28 13:46:15 -0700

    added user model

---
+++ models/User.js
@@ -0,0 +1,54 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../../config/connection');
+// const bcrypt = require("bcrypt");
+
+// Extending the sequelize model type to make columns in our sql db.
+class User extends Model {};
+
+User.init({
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
+        }
+    },
+    first_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            isAlpha:true
+        }
+    },
+    last_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            isAlpha:true
+        }
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [8]
+        }
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+}, {
+    sequelize,
+    // Hashes password so user login can't be accessed through db queries.
+    // hooks:{
+    //     beforeCreate:userObj=>{
+    //         userObj.password = bcrypt.hashSync(userObj.password,4);
+    //         return userObj;
+    //     }
+    // }
+})
+
+module.exports = User;
--- models/index.js
+++ models/index.js
@@ -1,2 +1,5 @@
 // Grab our database connection so we can define sequelize 'models' (=SQL tables) and their relationships to one another.
 const sequelize = require("../config/connection.js");
+
+// import our models to assign relationships in this file:
+const User = require('./User.js');
