3b0d53de0c65bb66fd79a8ec9143e97250a4a231
agtTwilight <lukasmacmillen@gmail.com>
2023-04-30 15:50:29 -0700

    added user based associations

--- models/index.js
+++ models/index.js
@@ -8,6 +8,16 @@ const Notification = require('./Notification.js');
 const Post = require('./Post.js');
 const User = require('./User.js');
 
+// User based associations:
+User.hasMany(Comment);
+Comment.belongsTo(User, {onDelete: "CASCADE", foreignKey: "user_id"});
+
+User.hasMany(Post);
+Post.belongsTo(User, {onDelete: "CASCADE", foreignKey: "user_id"});
+
+User.belongsToMany(Event, {through: 'User_Events'});
+Event.belongsToMany(User, {through: 'User_Events'});
+
 module.exports = {
     Comment,
     Event,
